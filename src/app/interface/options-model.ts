cc3e289e2135715eab1a7125924dd7f1eec5bdf3
马宇辉 <yuhui.ma@eoitek.com>
2018-12-19 19:31:51 +0800

    update interface

--- src/app/interface/options-model.ts
+++ src/app/interface/options-model.ts
@@ -1,12 +1,13 @@
+interface OptionsDataModel {
+  count: number; // 当前段的数值
+  width: number; // 当前段所占比例 0~100 总和应为100
+}
+
 export interface OptiopnsModel {
-  unit: string;
+  unit: string; // 单位
   step: number;
-  showInput: boolean;
-  min?: number;
-  max?: number;
-  data: Array<{
-    count: number;
-    width: number;
-    hoverText?: string;
-  }>;
+  showInput: boolean; // 是否显示input
+  min?: number; // 最小值
+  max?: number; // 最大值
+  data: OptionsDataModel[];
 }
